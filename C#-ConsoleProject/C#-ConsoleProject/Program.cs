9c7afec58bca7d9de08b8c2760ac5447a0895c30
Agshin N. Valiyev <agshinnv@code.edu.az>
2023-11-21 21:49:06 +0400

    ConsoleExtension and OperationTypes added

--- C#-ConsoleProject/C#-ConsoleProject/Program.cs
+++ C#-ConsoleProject/C#-ConsoleProject/Program.cs
@@ -1,6 +1,13 @@
-﻿using Spectre.Console;
+﻿using Service.Helpers.Extensions;
+using Spectre.Console;
 AnsiConsole.Write(
-    new FigletText("Console Project")
+    new FigletText("Welcome our Application")
         .LeftJustified()
         .Color(Color.LightGoldenrod2_2));
+
+
+static void GetMenues()
+{
+    ConsoleColor.Magenta.WriteConsole("Please select one option: Group operations: <1> Create, <2> Delete, <3> Edit, <4> GetById, <5> GetAll, <6> Search, <7> Sorting | Student operations : 8-Create, 9-Delete, 10-Edit, 11-GetById, 12-GetAll,13-Filter, 14-Search");
+}
 
